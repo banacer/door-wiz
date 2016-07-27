b7657feaa2638b69bf6e5aa96772126a1b5feb90
Nacer Khalil <nacerkhalil@gmail.com>
2016-07-27 10:36:31 -0500

    calibration done

--- src/large_scale experiment/featureExtractor/display_data.py
+++ src/large_scale experiment/featureExtractor/display_data.py
@@ -9,12 +9,15 @@ def run():
         line = ser.readline()
         #print line
         data = line.split(',')
-        calibration_a = 0.01703217
+        calibration_a = .01877037
         if len(data) != 3:
             continue
-        for i in range(3):
-             data[i] = calibration_a * float(data[i])
+        # for i in range(3):
+        #      data[i] = calibration_a * float(data[i])
         print data[0], data[1], data[2]
+        # print data[0], 216.65
+        # print data[1], 114.94
+        # print data[2].rstrip('\n\r'), 114.78
 
 if __name__ == '__main__':
     run()
